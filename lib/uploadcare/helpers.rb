68ecd0633699d3730f041b6a8d5afa4b7be7cf2a
Vadim Rastyagaev <abc@oktoberliner.ru>
2012-10-25 16:16:27 +0400

    hi, i'm a static error

--- lib/uploadcare/helpers.rb
+++ lib/uploadcare/helpers.rb
@@ -1,7 +1,7 @@
 module Uploadcare
   module Helpers
     def self.widget_url
-      "#{Uploadcare.default_setting[:static_url_base]}/uploadcare/uploadcare-#{Uploadcare.default_setting[:widget_version]}.min.js",
+      "#{Uploadcare.default_setting[:static_url_base]}/uploadcare/uploadcare-#{Uploadcare.default_setting[:widget_version]}.min.js"
     end
 
     def self.widget_script_tag
